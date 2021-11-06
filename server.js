bf55ccb892a908a95bf5b623ceaa8fd9c5684c3d
abdulhakim_altunkaya <altunkaya2@gmail.com>
2021-11-06 21:57:49 +0300

    heroku 24

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, './frontend')));
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./frontend/public/index.html"))
+})
 /*
 app.use(express.static(path.join(__dirname, './frontend/public')));
 */
